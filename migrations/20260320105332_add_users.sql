5873792e93aeb60dcf3e364c9bbaae394db6db67
Vadz-Danil <vadz.danil@gmail.com>
2026-03-23 20:28:37 +0200

    add migration for auth_service and fix compose.infra.yaml

--- migrations/20260320105332_add_users.sql
+++ migrations/20260320105332_add_users.sql
@@ -1,9 +1,32 @@
 -- +goose Up
--- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
+CREATE TABLE roles
+(
+    id         SERIAL PRIMARY KEY,
+    slug       VARCHAR(50)  NOT NULL UNIQUE,
+    name       VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE users
+(
+    id            UUID PRIMARY KEY         DEFAULT gen_random_uuid(),
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT         NOT NULL,
+    created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_roles
+(
+    user_id UUID    NOT NULL,
+    role_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_roles_role_id FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_user_roles_role_id ON user_roles(role_id);
 
 -- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
