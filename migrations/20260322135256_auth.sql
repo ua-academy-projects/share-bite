29f9127015b628c8678bba15b5bb2b619b48a21e
DimaTarasiuk <mr.tarasiuk@gmail.com>
2026-03-25 12:05:47 +0200

    Remove INSERT statements from auth.sql
    Removed initial data insertion for roles and users.

--- migrations/20260322135256_auth.sql
+++ migrations/20260322135256_auth.sql
@@ -21,18 +21,6 @@ CREATE TABLE auth.user_roles (
                                  PRIMARY KEY (user_id, role_id)
 );
 
-INSERT INTO auth.roles (slug, name)
-VALUES ('admin', 'Адміністратор'),
-       ('user', 'Користувач'),
-       ('business', 'Бізнес'),
-       ('moderator', 'Модератор');
-
-INSERT INTO auth.users (email, password_hash)
-VALUES (
-           'test@test.com',
-           '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
-       );
-
 -- +goose Down
 DROP TABLE IF EXISTS auth.user_roles;
 DROP TABLE IF EXISTS auth.users;
