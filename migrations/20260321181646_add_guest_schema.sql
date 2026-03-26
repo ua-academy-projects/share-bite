327acc2ee4bc4cd67b36fe70ba1f82a2f092d644
Ivan Popov <vanap387@gmail.com>
2026-03-26 15:16:36 +0200

    chore: update migration timestamps for correct order

--- migrations/20260321181646_add_guest_schema.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE SCHEMA IF NOT EXISTS guest;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-DROP SCHEMA IF EXISTS guest;
--- +goose StatementEnd
