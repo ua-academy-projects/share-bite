8f23f8385c6eb910226f0d1e12d163d8f3dc1292
durariri <rkozimova@gmail.com>
2026-03-28 12:22:09 +0200

    feat(customer): protect avatar upload and handle missing storage

--- internal/guest/handler/customer/get_by_username.go
+++ internal/guest/handler/customer/get_by_username.go
@@ -21,7 +21,7 @@ func (h *handler) getByUserName(c *gin.Context) {
 		return
 	}
 
-	resp := getByUserNameResponse{Customer: customerToResponse(customer)}
+	resp := getByUserNameResponse{Customer: customerToResponse(customer, h.storage)}
 	c.JSON(http.StatusOK, resp)
 }
 
