0e75054365d00cd2f9622c63b84b066eacb1758c
Ilya Maluk <145286412+IlyaMaluk@users.noreply.github.com>
2026-04-06 18:06:41 +0200

    Replace errwrap with fmt.Errorf for error handling

--- internal/guest/service/post/like.go
+++ internal/guest/service/post/like.go
@@ -3,19 +3,19 @@ package post
 import (
 	"context"
 
-	"github.com/ua-academy-projects/share-bite/pkg/errwrap"
+	"fmt"
 )
 
 func (s *service) Like(ctx context.Context, postID string, customerID string) error {
 	if err := s.postRepo.Like(ctx, postID, customerID); err != nil {
-		return errwrap.Wrap("like post in repository", err)
+		return fmt.Errorf("like post in repository: %w", err)
 	}
 	return nil
 }
 
 func (s *service) Unlike(ctx context.Context, postID string, customerID string) error {
 	if err := s.postRepo.Unlike(ctx, postID, customerID); err != nil {
-		return errwrap.Wrap("unlike post in repository", err)
+		return fmt.Errorf("unlike post in repository: %w", err)
 	}
 	return nil
 }
