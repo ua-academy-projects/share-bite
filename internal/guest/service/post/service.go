302aadcfa695338448ecd66dedee6077d80a9ac7
Riana <rkozimova@gmail.com>
2026-05-02 14:13:42 +0300

    test(post): update existing tests and add mentions coverage

--- internal/guest/service/post/service.go
+++ internal/guest/service/post/service.go
@@ -37,7 +37,6 @@ type VenueProvider interface {
 
 type service struct {
 	postRepo      postRepository
-	followRepo    followRepository
 	venueProvider VenueProvider
 	followRepo    follow.CustomerFollowRepository
 	customerRepo  customer.CustomerRepository
