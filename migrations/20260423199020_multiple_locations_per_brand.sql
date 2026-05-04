5f6910a29394ad504964d9d9121a5979fb0baa0d
Denys Shpak <shpak.denys.oleks@gmail.com>
2026-05-04 20:57:06 +0300

    fix: change migrations order and fix multiple locations migrations (#178)

--- migrations/20260423199020_multiple_locations_per_brand.sql
+++ migrations/20260423199020_multiple_locations_per_brand.sql
@@ -1,8 +1,7 @@
 -- +goose Up
-ALTER TABLE IF EXISTS business.org_units
-DROP CONSTRAINT IF EXISTS org_units_org_account_id_key;
-
-DO $$
+-- +goose StatementBegin
+ALTER TABLE IF EXISTS business.org_units DROP CONSTRAINT IF EXISTS org_units_org_account_id_key;
+DO $$ 
 BEGIN
     IF to_regclass('business.org_units') IS NULL THEN
         RETURN;
@@ -11,26 +10,18 @@ BEGIN
     ON business.org_units (org_account_id)
     WHERE profile_type = 'BRAND';
 END $$;
+-- +goose StatementEnd
 
 -- +goose Down
-DO $$
+-- +goose StatementBegin
+DO $$ 
 BEGIN
     IF to_regclass('business.org_units') IS NULL THEN
         RETURN;
     END IF;
-
-    IF EXISTS (
-        SELECT 1
-        FROM business.org_units
-        GROUP BY org_account_id
-        HAVING COUNT(*) > 1
-    ) THEN
-        RAISE EXCEPTION
-            'Rollback blocked: business.org_units has duplicate org_account_id (BRAND/VENUE).';
-    END IF;
-
+    -- Логика отката
     DROP INDEX IF EXISTS business.org_units_brand_owner_uidx;
-
     ALTER TABLE business.org_units
     ADD CONSTRAINT org_units_org_account_id_key UNIQUE (org_account_id);
 END $$;
+-- +goose StatementEnd
