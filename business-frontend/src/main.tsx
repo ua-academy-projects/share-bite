c7d586b8d5628f59783051deb700bb15b899b3ce
Mykola <smikola25@gmail.com>
2026-05-07 18:32:43 +0300

    feat:add CreatePost UI

--- business-frontend/src/main.tsx
+++ business-frontend/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
+import "./App.css";
+
+document.documentElement.classList.add("dark");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
